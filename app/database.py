d4268a9c32e4695bb50cbab0183c009890a49096
MSD365BC <160451788+MSD365BC@users.noreply.github.com>
2026-05-21 16:46:10 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+engine = create_engine("sqlite:///data/tickets.db")
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
