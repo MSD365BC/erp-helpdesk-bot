27d0094fc974aa4e8f7b5c4f58ab07b1ebd3f6c2
MSD365BC <160451788+MSD365BC@users.noreply.github.com>
2026-05-21 16:46:27 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.database import Base
 
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String)
+    username = Column(String)
+    category = Column(String)
+    description = Column(Text)
+    status = Column(String, default="OPEN")
