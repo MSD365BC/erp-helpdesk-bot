f8075b84651a04c58133772cf04032a7ca70586e
MSD365BC <160451788+MSD365BC@users.noreply.github.com>
2026-05-22 08:24:58 +0700

    Delete app directory

--- app/_init_.py
+++
@@ -1,2 +0,0 @@
-# erp-helpdesk-bot
-ERP Helpdesk
--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("8893526417:AAHHnTRuYl78vkLY5MdVsdQzkmSZooubN0E")
-ADMIN_ID = int(os.getenv("67129358"))
